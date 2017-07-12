9fd728c52eb1d7da58139910e974a42c03d9c021
preetikrp <preetikrp@gmail.com>
2017-07-12 13:16:50 -0500

    Exercise Classes.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,6 +10,91 @@ namespace Classes
     {
         static void Main(string[] args)
         {
+
+            Company ignify = new Company ("Preeti", DateTime.Now);
+            Console.WriteLine($"{ignify.Name} {ignify.CreatedOn}");
+            Employee hana = new Employee("Shreeti", "july 2nd", "Admin");
+            Employee tamela = new Employee("Eliza", "july 3rd", "Developer");
+            Employee amrita = new Employee("Priya", "july 4th", "BA");
+            ignify.AddEmployee(hana);
+            ignify.AddEmployee(tamela);
+            ignify.AddEmployee(amrita);
+            ignify.ListEmployees();
+
+      
+        }
+    }
+
+    public class Company
+    {
+   
+        /*
+            Some readonly properties
+        */
+        public string Name { get; }
+        public DateTime CreatedOn { get; }
+
+        // Create a property for holding a list of current employees
+        public List<Employee> Employees = new List<Employee>();
+
+        // Create a method that allows external code to add an employee
+        public void ListEmployees()
+        {
+            foreach(Employee employee in Employees)
+            {
+                Console.WriteLine(employee.Name);
+
+
+            }
+        }
+
+
+
+        public void AddEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+
+        }
+
+        public void RemoveEmployee(Employee employee)
+        {
+            Employees.Remove(employee);
+        }
+        // Create a method that allows external code to remove an employee
+
+        /*
+        
+            Create a constructor method that accepts two arguments:
+                1. The name of the company
+                2. The date it was created
+
+            The constructor will set the value of the public properties
+        */
+        public Company (string companyName, DateTime taco )
+        {
+            Name = companyName;
+            CreatedOn = taco;
+
+        }
+
+           
+           
+
+
+    }
+
+    public class Employee
+    {
+
+        public string Name;
+        public string Position;
+        public string StartDate;
+        public Employee(string employeeName, string startDate, string position)
+        {
+            Name = employeeName;
+            StartDate = startDate;
+            Position = position;
         }
     }
 }
+
